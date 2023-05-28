b75f74ff6b0a9bcbb549171efc3ac724b363fe9f
Mertcan Yüksel <mertcan.yuksel@secomtr.com>
2023-05-28 17:41:33 +0300

    gonderm

---
+++ Models/ClassIEnumerable/Showcase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using MVC5Library.Models;
+
+namespace MVC5Library.Models.ClassIEnumerable
+{
+    public class Showcase
+    {
+        public IEnumerable<TBLBook> ShowCaseBook { get; set; }
+        public IEnumerable<TBLAbout> ShowCaseAbout { get; set; }
+        public IEnumerable<TBLComment> ShowCaseComment { get; set; }
+    }
+}
